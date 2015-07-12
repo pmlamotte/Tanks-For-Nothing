021ae60ae1b9a7d26572bc5547632672c47a77a8
Paul LaMotte <pmlamotte@gmail.com>
2015-07-12 15:21:23 -0500

    Added player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+
+		CharacterController body = GetComponent<CharacterController>();
+
+		body.SimpleMove(new Vector3(x * speed, 0 , y * speed));
+	}
+}
