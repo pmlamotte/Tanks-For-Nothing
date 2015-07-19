e5ba52d94bbbe8bbdd74f0a253a1c9c7b1f3cc3b
Paul LaMotte <pmlamotte@gmail.com>
2015-07-19 13:16:15 -0500

    Added basic character attributes component that currently just contains health. This could be updated in the future to contain things like speed or other global attributes (like resistances?).

---
+++ Assets/Scripts/CharacterAttributes.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAttributes : MonoBehaviour {
+
+	public float health = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void ApplyDamage(float damage) {
+		health -= damage;
+		if (damage < 0) {
+			SendMessage("OnDeath");
+		}
+	}
+}
