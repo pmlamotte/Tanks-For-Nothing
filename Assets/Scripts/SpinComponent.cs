4f1ba4153788e34d862a4d4c202a59b8dabc1928
Paul LaMotte <pmlamotte@gmail.com>
2015-07-23 23:12:14 -0500

    Added spin component

---
+++ Assets/Scripts/SpinComponent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinComponent : MonoBehaviour {
+
+	public float spinAmount = 90;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 rot = this.gameObject.transform.rotation.eulerAngles;
+		rot.y += spinAmount * Time.deltaTime;
+		this.gameObject.transform.rotation = Quaternion.Euler(rot);
+	}
+}
